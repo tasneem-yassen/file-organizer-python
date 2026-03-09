76100f4333cf178fb905123322c73c550123e786
tasneem-yassen <tasneem5872@gmail.com>
2026-03-09 05:10:55 +0200

    write basic first step

--- organizer.py
+++ organizer.py
@@ -1,1 +1,13 @@
+import os
+import shutil
+import sys
 
+def organize_folder() : 
+    #to be continued 
+    return 
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python organize.py <folder_path>")
+    else:
+        organize_folder(sys.argv[1])
